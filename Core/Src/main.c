bff59b1a8d82d0d0bfa9c9b11938a4a0000068e0
silviabueno26 <167072151+silviabueno26@users.noreply.github.com>
2026-01-20 18:59:06 +0100

    pantalla
    Co-Authored-By: Lidia González <167072178+lidiagonzalezrubio03@users.noreply.github.com>
    Co-Authored-By: martacuellar <203874614+martacuellar@users.noreply.github.com>

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -2,33 +2,54 @@
 /**
   ******************************************************************************
   * @file           : main.c
-  * @brief          : Proyecto Simon Dice - STM32F407
+  * @brief          : main program body
   ******************************************************************************
   */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include <stdlib.h> // Para rand() y srand()
+#include <stdlib.h>
+#include <stdio.h>
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
 ADC_HandleTypeDef hadc1;
+
+I2C_HandleTypeDef hi2c1;
+
 TIM_HandleTypeDef htim2;
 
 /* USER CODE BEGIN PV */
 int pasos_por_nivel[5] = {2, 3, 4, 5, 6};
 int nivel_actual = 0;
-int secuencia[50]; // Almacena la secuencia de colores
+int secuencia[50];
 
-// LED
 uint16_t leds_juego[5] = {GPIO_PIN_0, GPIO_PIN_1, GPIO_PIN_2, GPIO_PIN_3, GPIO_PIN_4};
 #define LED_VERDE GPIO_PIN_5
 #define LED_ROJO  GPIO_PIN_6
 
-// Botones
-uint16_t botones[5] = {GPIO_PIN_0, GPIO_PIN_1, GPIO_PIN_10, GPIO_PIN_11, GPIO_PIN_12};
-
 volatile int boton_pulsado_flag = -1;
 volatile uint32_t ultimo_tiempo_pulsacion = 0;
+
+#define LCD_ADDR (0x27 << 1)
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -36,29 +57,83 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_ADC1_Init(void);
 static void MX_TIM2_Init(void);
-/* USER CODE BEGIN PFP */
-void iniciar_semilla_aleatoria(void);
-void reproducir_secuencia(int pasos, int velocidad);
-void efecto_victoria(void);
-void efecto_derrota(void);
+static void MX_I2C1_Init(void);
 int obtener_indice_boton(uint16_t GPIO_Pin);
+/* USER CODE BEGIN PFP */
+
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
-//Interrupciones (cuando se pulsa botón)
-void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
-{
-	uint32_t tiempo_actual = HAL_GetTick();
-	if (tiempo_actual - ultimo_tiempo_pulsacion >50){
-		boton_pulsado_flag = obtener_indice_boton(GPIO_Pin);
-		ultimo_tiempo_pulsacion = tiempo_actual;
-	}
-
+// LCD
+void lcd_send_cmd(char cmd) {
+    char data_u, data_l;
+    uint8_t data_t[4];
+    data_u = (cmd & 0xf0);
+    data_l = ((cmd << 4) & 0xf0);
+    data_t[0] = data_u | 0x0C; data_t[1] = data_u | 0x08;
+    data_t[2] = data_l | 0x0C; data_t[3] = data_l | 0x08;
+    HAL_I2C_Master_Transmit(&hi2c1, LCD_ADDR, (uint8_t *)data_t, 4, 100);
 }
 
-//convierte pin en indice
+void lcd_send_data(char data) {
+    char data_u, data_l;
+    uint8_t data_t[4];
+    data_u = (data & 0xf0);
+    data_l = ((data << 4) & 0xf0);
+    data_t[0] = data_u | 0x0D; data_t[1] = data_u | 0x09;
+    data_t[2] = data_l | 0x0D; data_t[3] = data_l | 0x09;
+    HAL_I2C_Master_Transmit(&hi2c1, LCD_ADDR, (uint8_t *)data_t, 4, 100);
+}
+
+void lcd_init(void) {
+    HAL_Delay(50);
+    lcd_send_cmd(0x30); HAL_Delay(5);
+    lcd_send_cmd(0x30); HAL_Delay(1);
+    lcd_send_cmd(0x32); HAL_Delay(10);
+    lcd_send_cmd(0x28); lcd_send_cmd(0x0C); lcd_send_cmd(0x01);
+    HAL_Delay(2);
+}
+
+void lcd_send_string(char *str) {
+    while (*str) lcd_send_data(*str++);
+}
+
+void lcd_put_cur(int row, int col) {
+    switch (row) {
+        case 0: col |= 0x80; break;
+        case 1: col |= 0xC0; break;
+    }
+    lcd_send_cmd(col);
+}
+
+void lcd_clear(void) {
+    lcd_send_cmd(0x01);
+    HAL_Delay(2);
+}
+
+//luces cuando se gana
+void barrido_celebracion(void) {
+    for(int vueltas = 0; vueltas < 5; vueltas++) {
+        for(int i = 0; i < 5; i++) {
+            HAL_GPIO_WritePin(GPIOA, leds_juego[i], GPIO_PIN_SET);
+            HAL_Delay(50);
+        }
+        HAL_GPIO_WritePin(GPIOA, LED_VERDE, GPIO_PIN_SET);
+        HAL_Delay(100);
+        HAL_GPIO_WritePin(GPIOA, 0x7F, GPIO_PIN_RESET); // Apaga PA0 a PA6
+        HAL_Delay(100);
+    }
+}
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
+    uint32_t tiempo_actual = HAL_GetTick();
+    if (tiempo_actual - ultimo_tiempo_pulsacion > 250) {
+        boton_pulsado_flag = obtener_indice_boton(GPIO_Pin);
+        ultimo_tiempo_pulsacion = tiempo_actual;
+    }
+}
+
 int obtener_indice_boton(uint16_t GPIO_Pin) {
     if(GPIO_Pin == GPIO_PIN_0) return 0;
     if(GPIO_Pin == GPIO_PIN_1) return 1;
@@ -68,39 +143,33 @@ int obtener_indice_boton(uint16_t GPIO_Pin) {
     return -1;
 }
 
-//ADC
-// Lee ruido electrico de un pin al aire para que el juego sea aleatorio
 void iniciar_semilla_aleatoria(void) {
     HAL_ADC_Start(&hadc1);
     HAL_ADC_PollForConversion(&hadc1, 100);
-    uint32_t ruido = HAL_ADC_GetValue(&hadc1);
-    srand(ruido);
+    srand(HAL_ADC_GetValue(&hadc1));
     HAL_ADC_Stop(&hadc1);
 }
 
 void reproducir_secuencia(int pasos, int velocidad) {
+    lcd_put_cur(1, 0);
+    lcd_send_string("¡MIRA BIEN LOS LEDS!");
     HAL_Delay(500);
     for(int i = 0; i < pasos; i++) {
         HAL_GPIO_WritePin(GPIOA, leds_juego[secuencia[i]], GPIO_PIN_SET);
         HAL_Delay(velocidad);
         HAL_GPIO_WritePin(GPIOA, leds_juego[secuencia[i]], GPIO_PIN_RESET);
-        HAL_Delay(400);
+        HAL_Delay(300);
     }
+    lcd_put_cur(1, 0);
+    lcd_send_string("TE TOCA JUGAR");
 }
 
 void efecto_derrota(void) {
+    lcd_clear();
+    lcd_send_string("FALLO, VUELVE A INTENTARLO");
     HAL_GPIO_WritePin(GPIOA, LED_ROJO, GPIO_PIN_SET);
-    HAL_Delay(1000);
+    HAL_Delay(2000);
     HAL_GPIO_WritePin(GPIOA, LED_ROJO, GPIO_PIN_RESET);
-}
-
-void efecto_victoria(void) {
-    for(int i=0; i<3; i++) {
-        HAL_GPIO_WritePin(GPIOA, LED_VERDE, GPIO_PIN_SET);
-        HAL_Delay(200);
-        HAL_GPIO_WritePin(GPIOA, LED_VERDE, GPIO_PIN_RESET);
-        HAL_Delay(200);
-    }
 }
 /* USER CODE END 0 */
 
@@ -110,22 +179,41 @@ void efecto_victoria(void) {
   */
 int main(void)
 {
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
   /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
   SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_ADC1_Init();
   MX_TIM2_Init();
-
+  MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
+  lcd_init();
+  lcd_send_string("  SIMON DICE  ");
+  lcd_put_cur(1, 0);
+  lcd_send_string("   ¡A JUGAR!   ");
 
   iniciar_semilla_aleatoria();
-
-
   HAL_TIM_Base_Start(&htim2);
-
-  // Parpadeo del principio
-  efecto_victoria();
+  HAL_Delay(2000);
   nivel_actual = 0;
   /* USER CODE END 2 */
 
@@ -133,95 +221,74 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-      int num_pasos = pasos_por_nivel[nivel_actual];
+    /* USER CODE END WHILE */
 
-      // Velocidad aumenta con el nivel
-      int velocidad = 1000 - (nivel_actual * 150);
-      if (velocidad < 400) velocidad = 400;
-
-      // secuencia nueva
-      for(int i=0; i<num_pasos; i++){
-          secuencia[i] = rand() % 5;
-      }
-
-      reproducir_secuencia(num_pasos, velocidad);
-      boton_pulsado_flag = -1;
-      HAL_Delay(50);
-
-      //pasos jugador
-      int error = 0;
-      for(int i = 0; i < num_pasos; i++) {
-          boton_pulsado_flag = -1;
-
-          // REVISAR ESTO
-          __HAL_TIM_SET_COUNTER(&htim2, 0); // Reiniciar cronometro
-
-          // Esperar pulsar boton o 30 segundos
-          while(boton_pulsado_flag == -1) {
-              if (__HAL_TIM_GET_COUNTER(&htim2) > 30000) {
-                  error = 1;
-                  break;
-              }
-          }
-
-          if(error) break;
-
-          // acierto
-          if(boton_pulsado_flag == secuencia[i]) {
-              // enciende el led pulsado
-              HAL_GPIO_WritePin(GPIOA, leds_juego[boton_pulsado_flag], GPIO_PIN_SET);
-              HAL_Delay(300);
-              HAL_GPIO_WritePin(GPIOA, leds_juego[boton_pulsado_flag], GPIO_PIN_RESET);
-          } else {
-              error = 1; //mal
-              break;
-          }
-      }
-
-      // solucion
-      if(error) {
-          efecto_derrota();
-          nivel_actual = 0; // Reiniciar
-          HAL_Delay(1000);
-      } else {
-          // juego ganado
-          HAL_GPIO_WritePin(GPIOA, LED_VERDE, GPIO_PIN_SET);
-          HAL_Delay(500);
-          HAL_GPIO_WritePin(GPIOA, LED_VERDE, GPIO_PIN_RESET);
-
-          if(nivel_actual < 4) {
-              nivel_actual++;
-          } else {
-              efecto_victoria(); // efecto ganar
-              nivel_actual = 0;
-          }
-          HAL_Delay(1000);
-      }
+    /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
 }
 
-void SystemClock_Config(void) {
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
   __HAL_RCC_PWR_CLK_ENABLE();
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-  HAL_RCC_OscConfig(&RCC_OscInitStruct);
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-  HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0);
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
 }
 
-static void MX_ADC1_Init(void) {
+/**
+  * @brief ADC1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ADC1_Init(void)
+{
+
+  /* USER CODE BEGIN ADC1_Init 0 */
+
+  /* USER CODE END ADC1_Init 0 */
+
   ADC_ChannelConfTypeDef sConfig = {0};
+
+  /* USER CODE BEGIN ADC1_Init 1 */
+
+  /* USER CODE END ADC1_Init 1 */
+
+  /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
+  */
   hadc1.Instance = ADC1;
   hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV2;
   hadc1.Init.Resolution = ADC_RESOLUTION_12B;
@@ -234,61 +301,188 @@ static void MX_ADC1_Init(void) {
   hadc1.Init.NbrOfConversion = 1;
   hadc1.Init.DMAContinuousRequests = DISABLE;
   hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
-  HAL_ADC_Init(&hadc1);
+  if (HAL_ADC_Init(&hadc1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
+  */
   sConfig.Channel = ADC_CHANNEL_7;
   sConfig.Rank = 1;
   sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
-  HAL_ADC_ConfigChannel(&hadc1, &sConfig);
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ADC1_Init 2 */
+
+  /* USER CODE END ADC1_Init 2 */
+
 }
 
-static void MX_TIM2_Init(void) {
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.ClockSpeed = 100000;
+  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
+}
+
+/**
+  * @brief TIM2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM2_Init(void)
+{
+
+  /* USER CODE BEGIN TIM2_Init 0 */
+
+  /* USER CODE END TIM2_Init 0 */
+
   TIM_ClockConfigTypeDef sClockSourceConfig = {0};
   TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM2_Init 1 */
+
+  /* USER CODE END TIM2_Init 1 */
   htim2.Instance = TIM2;
-  htim2.Init.Prescaler = 15999;
+  htim2.Init.Prescaler = 0;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim2.Init.Period = 4294967295;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  HAL_TIM_Base_Init(&htim2);
+  if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
   sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
-  HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig);
+  if (HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
   sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
   sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-  HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig);
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM2_Init 2 */
+
+  /* USER CODE END TIM2_Init 2 */
+
 }
 
-static void MX_GPIO_Init(void) {
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
   GPIO_InitTypeDef GPIO_InitStruct = {0};
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
 
-  // Salidas
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3
-                          |GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6, GPIO_PIN_RESET);
-  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3
-                          |GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6;
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1|GPIO_PIN_0|GPIO_PIN_2|GPIO_PIN_6
+                          |GPIO_PIN_5|GPIO_PIN_3, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PA1 PA0 PA2 PA6
+                           PA5 PA3 */
+  GPIO_InitStruct.Pin = GPIO_PIN_1|GPIO_PIN_0|GPIO_PIN_2|GPIO_PIN_6
+                          |GPIO_PIN_5|GPIO_PIN_3;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  // Entradas
-  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_10|GPIO_PIN_11|GPIO_PIN_12;
+  /*Configure GPIO pins : PB12 PB1 PB0 PB10
+                           PB11 */
+  GPIO_InitStruct.Pin = GPIO_PIN_12|GPIO_PIN_1|GPIO_PIN_0|GPIO_PIN_10
+                          |GPIO_PIN_11;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
   GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-  // Activar interrupciones en NVIC
+  /* EXTI interrupt init*/
   HAL_NVIC_SetPriority(EXTI0_IRQn, 0, 0);
   HAL_NVIC_EnableIRQ(EXTI0_IRQn);
+
   HAL_NVIC_SetPriority(EXTI1_IRQn, 0, 0);
   HAL_NVIC_EnableIRQ(EXTI1_IRQn);
+
   HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
   HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
 }
 
-void Error_Handler(void) {
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
-  while (1) {}
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
 }
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
